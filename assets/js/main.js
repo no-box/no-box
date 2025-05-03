55e3256d8ef393d5f7e2d72a7d7dad267412dfa4
Brux-Stream <brux.stream@gmail.com>
2025-05-03 15:58:35 +0200

    “NoBox”

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,1 @@
 /* Prazno */
-document.querySelectorAll('.image-link').forEach(link => {
-  link.addEventListener('click', function(e) {
-      e.preventDefault();
-      const imagePath = this.getAttribute('data-image');
-      document.getElementById('image-display').style.backgroundImage = `url('${imagePath}')`;
-  });
-});
-
